dcad6c05b4e9a9c0a899721bcde385aefc035721
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-17 18:18:54 +0200

    starGame function added
    used methods like:
    setInterval, setTimeout; clearinterval
    and addeventListener to start the game by clicking on the start button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const holes = document.querySelectorAll('.hole')
 const scoreBoard = document.querySelector('.score')
 const moles = document.querySelectorAll('.mole')
-const countdownboard = document.querySelector('.countdown')
+const countdownBoard = document.querySelector('.countdown')
 const startButton = document.querySelector('.startButton')
 
 let lastHole
@@ -28,4 +28,29 @@ function popOut() {
         if (!timeUp) popOut()
     }, time)
 }
- popOut()
+
+
+ function startGame() {
+    countdown = timeLimit/1000
+    scoreBoard.textContent = 0
+    scoreBoard.style.display = 'block'
+    countdownBoard.textContent = countdown
+    timeUp = false
+    score = 0
+    popOut()
+    setTimeout(function(){
+        timeUp = true
+    }, timeLimit)
+
+    let startCountdown = setInterval(function(){
+        countdown -= 1
+        countdownBoard.textContent = countdown
+        if (countdown < 0) {
+            countdown = 0
+            clearInterval(startCountdown)
+            countdownBoard.textContent = 'Times UP!! thank you master! This is the Way!'
+        }
+    }, 1000)
+ }
+
+ startButton.addEventListener('click', startGame )
