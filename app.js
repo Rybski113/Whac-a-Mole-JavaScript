3a8a530d9f832707d2d668ebea35f0912b9b54d8
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2023-10-17 17:50:03 +0200

    random appear in holes
    two function were made.
    One for picking random holes and second for yoda popOut from the specific hole

--- app.js
+++ app.js
@@ -3,3 +3,29 @@ const scoreBoard = document.querySelector('.score')
 const moles = document.querySelectorAll('.mole')
 const countdownboard = document.querySelector('.countdown')
 const startButton = document.querySelector('.startButton')
+
+let lastHole
+let timeUp = false
+let timeLimit = 20000
+let score = 0
+let countdown
+
+function pickRandomHole(holes) {
+   const randomHole = Math.floor(Math.random() * holes.length)
+   const hole = holes[randomHole]
+   if(hole === lastHole) {
+    return pickRandomHole(holes)
+   }
+   lastHole = hole
+   return hole
+}
+function popOut() {
+    const time = Math.random() * 1300 + 400
+    const hole = pickRandomHole(holes)
+    hole.classList.add('up')
+    setTimeout(function() {
+        hole.classList.remove('up')
+        if (!timeUp) popOut()
+    }, time)
+}
+ popOut()
